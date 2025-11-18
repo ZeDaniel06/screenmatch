f475859d34c52c61fb9b5706b0f52b5c5521f3a1
ZeDaniel06 <silvadan752@gmail.com>
2025-11-18 17:47:09 -0300

    Implementada desserialização de json com Jackson

--- src/main/java/br/com/zedaniel/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/zedaniel/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.zedaniel.screenmatch;
 
+import br.com.zedaniel.screenmatch.model.DadosSerie;
 import br.com.zedaniel.screenmatch.service.ConsumoApi;
+import br.com.zedaniel.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,11 +17,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var url = "https://www.omdbapi.com/?t=breaking+bad&apikey=93684a76&season=1";
+		var url = "https://www.omdbapi.com/?t=breaking+bad&apikey=93684a76";
 		var json = consumoApi.obterDados(url);
 		System.out.println(json);
-		url = "https://coffee.alexflipnote.dev/random.json";
-		json = consumoApi.obterDados(url);
-		System.out.println(json);
+		var conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+//		url = "https://coffee.alexflipnote.dev/random.json";
+//		json = consumoApi.obterDados(url);
+//		System.out.println(json);
 	}
 }
---
+++ src/main/java/br/com/zedaniel/screenmatch/service/ConverteDados.java
@@ -0,0 +1,15 @@
+package br.com.zedaniel.screenmatch.service;
+
+import br.com.zedaniel.screenmatch.model.DadosSerie;
+import tools.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados{
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        return mapper.readValue(json, classe);
+    }
+}

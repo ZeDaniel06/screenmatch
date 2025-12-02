95497525539b84987f9b25fb8b3c8356ada1c2e7
ZeDaniel06 <silvadan752@gmail.com>
2025-12-02 15:28:12 -0300

    Adicionada integração com api do chatGPT para tradução de sinopses

--- src/main/java/br/com/zedaniel/screenmatch/principal/Principal.java
+++ src/main/java/br/com/zedaniel/screenmatch/principal/Principal.java
@@ -103,45 +103,4 @@ public class Principal {
 
 
 
-/*
-package br.com.alura.screenmatch.service;
-
-import com.theokanning.openai.completion.CompletionRequest;
-import com.theokanning.openai.service.OpenAiService;
-
-public class ConsultaChatGPT {
-public static String obterTraducao(String texto) {
-OpenAiService service = new OpenAiService("cole aqui sua chave da OpenAI");
-
-CompletionRequest requisicao = CompletionRequest.builder()
-.model("gpt-3.5-turbo-instruct")
-.prompt("traduza para o português o texto: " + texto)
-.maxTokens(1000)
-.temperature(0.7)
-.build();
-
-var resposta = service.createCompletion(requisicao);
-return resposta.getChoices().get(0).getText();
-}
-}
-
-
-
-
-///////////////////
-
-
-<dependency>
-<groupId>com.theokanning.openai-gpt3-java</groupId>
-<artifactId>service</artifactId>
-<version>0.14.0</version>
-</dependency>
- */
- */
- */
-* */
-
-
-
-
 

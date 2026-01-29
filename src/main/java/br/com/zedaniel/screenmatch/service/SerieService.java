00942a03731ebe2c3ecde7a172c09fb257eae850
ZeDaniel06 <silvadan752@gmail.com>
2026-01-29 07:08:38 -0300

    Service de series

---
+++ src/main/java/br/com/zedaniel/screenmatch/service/SerieService.java
@@ -0,0 +1,22 @@
+package br.com.zedaniel.screenmatch.service;
+
+import br.com.zedaniel.screenmatch.dto.SerieDTO;
+import br.com.zedaniel.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+    @Autowired
+    private SerieRepository repositorio;
+
+    public List<SerieDTO> obterTodasAsSeries(){
+        return repositorio.findAll().stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
+                        s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+}

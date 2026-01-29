11426c505c50e2f7b784bf5b9461491734d63433
ZeDaniel06 <silvadan752@gmail.com>
2026-01-29 07:48:20 -0300

    Mais endpoints, o de top 5 series

--- src/main/java/br/com/zedaniel/screenmatch/service/SerieService.java
+++ src/main/java/br/com/zedaniel/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.zedaniel.screenmatch.service;
 
 import br.com.zedaniel.screenmatch.dto.SerieDTO;
+import br.com.zedaniel.screenmatch.model.Serie;
 import br.com.zedaniel.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,15 @@ public class SerieService {
     private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasAsSeries(){
-        return repositorio.findAll().stream()
+        return converterDados(repositorio.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converterDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    private List<SerieDTO> converterDados(List<Serie> series){
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
                         s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());

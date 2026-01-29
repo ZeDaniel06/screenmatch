28765f76b4660d95db73257e87cf90f26b7eb982
ZeDaniel06 <silvadan752@gmail.com>
2026-01-29 11:43:56 -0300

    Dados de series individuais

--- src/main/java/br/com/zedaniel/screenmatch/service/SerieService.java
+++ src/main/java/br/com/zedaniel/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,6 +31,18 @@ public class SerieService {
     }
 
     public List<SerieDTO> obterLancamentos() {
-        return converterDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+        return converterDados(repositorio.encontrarEpisodiosMaisRecentes());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+        if(serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
+                    s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+
+        return null;
+
     }
 }
